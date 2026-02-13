97e2cb959d3529a2450c975f13957db06fc60784
moonyahah <27100459@lums.edu.pk>
2026-02-13 19:02:13 +0500

    please work

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let yesSize = 18;
 
-function showStory() {
+function showPage2() {
     document.getElementById("page1").classList.add("hidden");
     document.getElementById("page2").classList.remove("hidden");
 }
 
-function showQuestion() {
+function showPage3() {
     document.getElementById("page2").classList.add("hidden");
     document.getElementById("page3").classList.remove("hidden");
+}
+
+function showPage4() {
+    document.getElementById("page3").classList.add("hidden");
+    document.getElementById("page4").classList.remove("hidden");
+}
+
+function showPage5() {
+    document.getElementById("page4").classList.add("hidden");
+    document.getElementById("page5").classList.remove("hidden");
+}
+
+function showPage6() {
+    document.getElementById("page5").classList.add("hidden");
+    document.getElementById("page6").classList.remove("hidden");
     startHearts();
 }
 
 function yesClicked() {
-    document.getElementById("page3").classList.add("hidden");
+    document.getElementById("page6").classList.add("hidden");
     document.getElementById("success").classList.remove("hidden");
 }
 
